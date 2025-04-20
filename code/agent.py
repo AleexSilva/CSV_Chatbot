ea2d3a63ebf8f45f6ef172f0b01c1789b3044e98
AleexSilva <silva.alexis94@gmail.com>
2025-04-20 19:45:31 -0300

    Create Agent

---
+++ code/agent.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from decouple import config
+import warnings
+warnings.filterwarnings('ignore')
+
+#Lnagchain
+from langchain import OpenAI
+from langchain_experimental.agents import create_csv_agent
+
+df = pd.read_csv('../data/financial_data.csv')
+df.head()
+
+openai_api_key = config('OPENAI_API_KEY')
+
+agent = create_csv_agent(
+    OpenAI(api_key=openai_api_key), 
+    '../data/financial_data.csv', 
+    verbose=True
+)
