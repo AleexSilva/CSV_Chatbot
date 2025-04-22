fb0ec38d3adf68bd28521ca62066f7cdd59a0fc3
AleexSilva <silva.alexis94@gmail.com>
2025-04-22 11:41:41 -0300

    upload file

--- code/app.py
+++ code/app.py
@@ -14,3 +14,12 @@ openai_api_key = config('OPENAI_API_KEY')
 st.title('CSV Data - RAG Chatbot')
 
 uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+
+
+if uploaded_file is not None:
+    csv_file_path = uploaded_file.name
+    
+    with open(csv_file_path, 'wb') as file:
+        file.write(uploaded_file.getbuffer())
+        
+    df = pd.read_csv(csv_file_path)
